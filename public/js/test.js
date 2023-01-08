275bc0acfe3d7123df88518143b4cdb0320d8c62
unknown <rilrahe@gmail.com>
2023-01-08 14:59:59 -0600

    working on buttion

--- public/js/test.js
+++ public/js/test.js
@@ -3,3 +3,20 @@ const teamName = getElementById()
 function updateFavoriteTeam(element) {
     const teamName = element
 }
+
+
+const button = document.querySelector('#log-button');
+
+// Add an event listener for the click event
+button.addEventListener('click', function() {
+  // Send a POST request to the back end with the information you want to store
+  fetch('/api/user-routes', {
+    method: 'POST',
+    body: JSON.stringify({
+      data: "user log in info"
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+});
--- routes/api/team-routes.js
+++ routes/api/team-routes.js
@@ -1,28 +1,22 @@
 const fetch = require('node-fetch');
 const router = require('express').Router();
 
-router.get("/",(req,res)=>{
-fetch('https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2022',{
-    method:"GET",
-    headers: {
-        "X-RapidAPI-Key":"eeb456aa43msh70da8f932010539p1cd7cbjsn3bd3befcf45b",
-        "X-RapidAPI-Host":"nfl-team-stats.p.rapidapi.com",
+router.get("/", (req, res) => {
+  fetch(
+    'https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2022',
+    {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'eeb456aa43msh70da8f932010539p1cd7cbjsn3bd3befcf45b',
+        'X-RapidAPI-Host': 'nfl-team-stats.p.rapidapi.com',
+      },
     }
-})
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // localStorage.setItem("teamWinStatsList",data._embedded.teamWinStatsList)
+      res.redirect('http://localhost:3001/teams');
+    });
+});
 
-
-
-.then((response)=>
-    response.json()
-).then((data)=>{
-    // localStorage.setItem("teamWinStatsList",data._embedded.teamWinStatsList)
-    res.send(data)
-})
-}) 
-
- // {
-// 	"_embedded": {
-// 		"teamWinStatsList": [
-// 			{
-// look at file structure for last mini-project 
 module.exports = router;
