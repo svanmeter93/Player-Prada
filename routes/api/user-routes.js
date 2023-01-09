dd3a1c3387fdbb9b1745b65c955652bd7aa2cf95
unknown <rilrahe@gmail.com>
2023-01-08 18:14:05 -0600

    got the log in to work

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,49 +1,71 @@
-const user = require('express').Router();
-const withauth =require('../../utils/withauth');
-const {User} = require("../../models");
+const user = require("express").Router();
+const withauth = require("../../utils/withauth");
+const { User } = require("../../models");
 
-user.post("/",async(req,res)=>{
+user.post("/", async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const newUser = await User.create({
+      username,
+      password,
+      fav_team_name: null,
+    });
+    req.session.save(() => {
+      req.session.logged_in = true;
+      req.session.user_id = newUser.id;
+      res.json(newUser);
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+user.post("/login", async (req, res) => {
+  try {
+    const userData = await User.findOne({
+      where: { username: req.body.username },
+    });
+    if (!userData) {
+      return res.status(404).json({ message: "User not found!" });
+    }
+    const correctPW = await userData.checkPassword(req.body.password);
+    if (!correctPW) {
+      return res.status(400).json({ message: "Incorrect password!" });
+    }
+    req.session.save(() => {
+      (req.session.logged_in = true), (req.session.user_id = userData.id);
+      res.json({ message: "Logged in!" });
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+user.get("/logout", (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.redirect("/");
+    });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+user.put("/favoriteteam", async (req, res) => {
     try {
-        const {username,password} = req.body
-        const newUser = await User.create({username,password,fav_team_name:null})
-        req.session.save(()=>{
-            req.session.logged_in=true
-            req.session.user_id=newUser.id
-            res.json(newUser)
-        })
-    } catch (error) {
-        res.status(400).json(error)
-    }
-});
+        const updatedUser = await User.update(
+        { fav_team_name: req.body.teamName },
+        { where: { id: req.session.user_id } }
+      );
+      if (updatedUser) {
+        res.json(updatedUser)
+      } else {
+       return res.status(404).json({message:'User not found!'})
+      }
 
-user.post("/login",async(req,res)=>{
-    try{
-        const userData = await User.findOne({where:{username:req.body.username}})
-        if (!userData) {
-            return res.status(404).json({message:"User not found!"})
-        } 
-        const correctPW = await userData.checkPassword(req.body.password)
-        if (!correctPW) {
-            return res.status(400).json({message:"Incorrect password!"})
-        }
-        req.session.save(()=> {
-            req.session.logged_in=true
-        res.json({message:'Logged in!'})
-        }
-        )
-    }
-    catch (error) {
-        res.status(500).json(error)}
-});
-
-user.get('/logout', (req, res) => {
-    if (req.session.logged_in) {
-        req.session.destroy(()=>{
-            res.redirect('/');
-        })
-    } else {
-        res.redirect('/login');
+    } catch (err){
+        res.status(500).json(err)
     }
 });
 // write route to update users favorite team name
-module.exports=user;
+module.exports = user;
