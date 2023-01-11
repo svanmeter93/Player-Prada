8be02c8c272cbd10d386902e93a869ed468c5dbe
svanmeter93 <107381935+svanmeter93@users.noreply.github.com>
2023-01-10 18:23:27 -0600

    "delete clutter & env changes"

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,6 @@
 const router = require('express').Router();
-const teamRoutes = require('./team-routes')
-const scoreRoutes = require ('./scores-routes')
 const userRoutes = require ('./user-routes')
 
-router.use('/teams', teamRoutes);
-router.use('/scores',scoreRoutes);
 router.use('/users',userRoutes);
 
 module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,7 +2,7 @@ const fetch = require('node-fetch');
 const html = require('express').Router();
 const withauth =require('../utils/withauth');
 const {User} = require("../models");
-
+require("dotenv").config()
 html.get("/",withauth,(req,res)=>{
     res.redirect("teams")
 })
@@ -11,8 +11,8 @@ html.get("/teams",withauth, async(req,res)=>{
     fetch('https://nfl-team-stats.p.rapidapi.com/v1/nfl-stats/teams/win-stats/2022',{
     method:"GET",
     headers: {
-        "X-RapidAPI-Key":"eeb456aa43msh70da8f932010539p1cd7cbjsn3bd3befcf45b",
-        "X-RapidAPI-Host":"nfl-team-stats.p.rapidapi.com",
+        "X-RapidAPI-Key":process.env.X_RapidAPI_Key,
+        "X-RapidAPI-Host":process.env.X_RapidAPI_Host,
     }
 })
 .then((response)=>
